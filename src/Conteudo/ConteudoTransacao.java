884bb34deae0cece62402a2e24ad7a7ae7ad4665
patrick-fed <fedrigo.patrick89@gmail.com>
2026-06-24 18:47:19 -0300

    Validação, espero que funcione

--- src/Conteudo/ConteudoTransacao.java
+++ src/Conteudo/ConteudoTransacao.java
@@ -1,4 +1,20 @@
 package Conteudo;
 
-public class ConteudoTransacao {
+import interfaces.Conteudo;
+
+public class ConteudoTransacao implements Conteudo {
+    private String remetente;
+    private String destinatario;
+    private double valor;
+
+    public ConteudoTransacao(String remetente, String destinatario, double valor) {
+        this.remetente = remetente;
+        this.destinatario = destinatario;
+        this.valor = valor;
+    }
+
+    @Override
+    public String serializarParaHash() {
+        return "TX:" + remetente + "->" + destinatario + ":" + valor;
+    }
 }
--- src/Validacao/ValidacaoCadeia.java
+++ src/Validacao/ValidacaoCadeia.java
@@ -1,4 +1,36 @@
 package Validacao;
 
+import interfaces.CalculadoraHash;
+import model.Bloco;
+import model.Cadeia;
+import java.util.List;
+
 public class ValidacaoCadeia {
+    private CalculadoraHash calculadoraHash;
+
+    public ValidacaoCadeia(CalculadoraHash calculadoraHash) {
+        this.calculadoraHash = calculadoraHash;
+    }
+
+    public boolean isValida(Cadeia cadeia) {
+        List<Bloco> blocos = cadeia.getBlocos();
+
+        for (int i = 1; i < blocos.size(); i++) {
+            Bloco blocoAtual = blocos.get(i);
+            Bloco blocoAnterior = blocos.get(i - 1);
+
+            if (!blocoAtual.getHashAnterior().equals(blocoAnterior.getHashBloco())) {
+                System.out.println("Erro de link no bloco " + blocoAtual.getId());
+                return false;
+            }
+
+
+            String hashRecalculado = calculadoraHash.calcular(blocoAtual.obterDadosParaHash());
+            if (!blocoAtual.getHashBloco().equals(hashRecalculado)) {
+                System.out.println("Conteudo adulterado detectado no bloco " + blocoAtual.getId());
+                return false;
+            }
+        }
+        return true;
+    }
 }
